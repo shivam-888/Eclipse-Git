421a5acb9fa022cee3622f60d757b1e15309d17b
Shivam Kumar <newshivam888@gmail.com>
2022-04-07 22:40:39 +0530

    initial commit

---
+++ GIT Eclipse Intro/src/Difference.java
@@ -0,0 +1,7 @@
+
+public class Difference {
+	public static void main(String[] args) {
+		int a=43,b=21;
+		System.out.println("DIFFERENCE: "+(a-b));
+	}
+}
---
+++ GIT Eclipse Intro/src/Sum.java
@@ -0,0 +1,7 @@
+
+public class Sum {
+	public static void main(String[] args) {
+		int a=5,b=6;
+		System.out.println("SUM: "+(a+b));
+	}
+}
